ed436361bef9bed1f938b9094b594f32f89f4aa6
Sira <miroslavsmrcka@protonmail.com>
2020-09-06 13:31:16 +0200

    Fixed bugs
    -Fixed individual Chrome window per e-mail
    -Fixed "only-paste" case
    -Replaced message for No breaches result
    -Added ratelimit sleep prevention
    -Added --only_valid argument to output non-empty breach results

--- leekyleaky.py
+++ leekyleaky.py
@@ -1,14 +1,21 @@
 # This unofficial tool serves as standard HIBP search (no API key needed)
 # Author: Sira
-# ChromeDriver is required, download here: https://chromedriver.chromium.org/index.html. Dont't forget to add it to system's PATH
+# ChromeDriver is required, download here: https://chromedriver.chromium.org/. Dont't forget to add it to system's PATH
 
 import sys
 import os.path
 import json
 import requests
+from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+def driver():
+    global driver
+    chrome_opt = webdriver.ChromeOptions()
+    chrome_opt.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=chrome_opt)
+    driver.set_window_position(-10000,0)
 
 def pasteExists(urlAdd):
     r = requests.get(urlAdd)
@@ -18,23 +25,20 @@ def pasteExists(urlAdd):
         return False
 
 def checkAddress(email):
-    global driver
-    chrome_opt = webdriver.ChromeOptions()
-    chrome_opt.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=chrome_opt)
-    driver.set_window_position(-10000,0)
     Url = 'https://haveibeenpwned.com/unifiedsearch/' + email
     driver.get(Url)
-    print("\n==Results for " + email + "==")
     try:
         driver.find_element_by_xpath('html/body/pre')
     except NoSuchElementException:
-        print("No breaches have been found!")
+        if "--only_valid" not in sys.argv:
+            print("No breaches have been found for " + email + "!")
     else:
+        print("\n==Results for " + email + "==")
         array = json.loads(driver.find_element_by_xpath('html/body/pre').text)
-        for i in range(len(array["Breaches"])):
-            print("Title: " + array["Breaches"][i]["Title"])
-            print("-Date: " + array["Breaches"][i]["BreachDate"])
+        if array["Breaches"] is not None:
+            for i in range(len(array["Breaches"])):
+                print("Title: " + array["Breaches"][i]["Title"])
+                print("-Date: " + array["Breaches"][i]["BreachDate"])
         if array["Pastes"] is not None:
             print("\nSearching for pastes that are up...")
             for j in range(len(array["Pastes"])):
@@ -46,7 +50,8 @@ def checkAddress(email):
                     binUrl = "https://pastebin.com/raw/" + pastes
                     if pasteExists(binUrl):
                         print(binUrl)
-    print("\n==End of results for " + email + "==")
+        print("\n==End of results for " + email + "==")
+    sleep(2) # Prevents ratelimiting ban
 
 def readLinesInFile(fileemails):
     fileMails = open(fileemails, 'r')
@@ -58,6 +63,7 @@ def main():
     if len(sys.argv)-1 == 0:
         print("Error: No files nor e-mail addresses have been specified.\n\nUsage:\t leekyleaky.py <path to file with e-mail address inside/e-mail address>\nExample: leekyleaky.py example@example.org example.txt\n\nYou can specify as many paths or addresses as you like.")
     else:
+        driver()
         for i in range(1, len(sys.argv)):
             if os.path.isfile(sys.argv[i]):
                 readLinesInFile(sys.argv[i])
